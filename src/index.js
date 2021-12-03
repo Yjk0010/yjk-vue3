52074b0f0f7ca91f685f1787d601d80526134de2
yanjk <yanjk@hzzh.com>
2021-12-03 18:02:12 +0800

    lint

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ if (NaN === 1) {
   console.log(123456);
   console.log(123456);
   console.log(123456);
+  console.log(123456);
+  console.log(123456);
+  console.log(123456);
+  console.log(123456);
+  console.log(123456);
 }
 const abc = {
   abc: 123,
